a65f9340ac1d017de4947ecf4dd4149fff3395aa
Benjamin Berkei <benjamin.berkei.ext@adevinta.com>
2023-03-21 15:53:24 +0100

    refactor: change package structure

---
+++ src/homework/sandro/calculator/Taschenrechner.java
@@ -0,0 +1,117 @@
+package homework.sandro.calculator;
+
+import java.util.Scanner;
+
+public class Taschenrechner {
+
+
+    public static void main(String[] args) {
+        System.out.println("*****+++++Taschenrechner Premium Deluxe 3000 (s Series)+++++*****");
+        System.out.println();
+
+        boolean repeat = true;
+        float memory = 0.0f;
+        float zahl = readZahl("Erste Zahl   ");
+
+        while (repeat) {
+            String operator = readOperator("Operator     ");
+            if (operator.equals("exit")) {
+                repeat = false;
+            } else if (operator.equals("M+")) {
+                memory += zahl;
+            } else if (operator.equals("M-")) {
+                memory -= zahl;
+            } else if (operator.equals("MR")) {
+                zahl = memory;
+                System.out.println("Ergebnis     : " + zahl);
+            } else if (operator.equals("MC")) {
+                memory = 0;
+            } else if (operator.equals("MS")) {
+                memory = zahl;
+            } else {
+                float zahl2 = readZahl("Nächste Zahl ");
+                float wert = 0.0f;
+
+                if (operator.equals("+")) {
+                    wert = zahl + zahl2;
+                } else if (operator.equals("-")) {
+                    wert = zahl - zahl2;
+                } else if (operator.equals("*")) {
+                    wert = zahl * zahl2;
+                } else if (operator.equals("^")) {
+                    wert = 1;
+                    for (int i = 0; i < zahl2; i++) {
+                        wert *= zahl;
+                    }
+                } else if (operator.equals("/")) {
+                    wert = zahl / zahl2;
+                } else {
+                    System.out.println("Unknown operator");
+                }
+                System.out.println("Ergebnis     : " + wert);
+                zahl = wert;
+            }
+        }
+
+        System.out.println("Auf Wiedersehen!");
+    }
+
+    public static String readOperator(String message) {
+        Scanner sc = new Scanner(System.in);
+        String operator = "";
+        boolean firstTry = true;
+
+        while (
+                !operator.equals("+") &&
+                        !operator.equals("-") &&
+                        !operator.equals("*") &&
+                        !operator.equals("/") &&
+                        !operator.equals("^") &&
+                        !operator.equals("M+") &&
+                        !operator.equals("M-") &&
+                        !operator.equals("MC") &&
+                        !operator.equals("MR") &&
+                        !operator.equals("MS") &&
+                        !operator.equals("exit")
+        ) {
+            if (!firstTry) {
+                System.out.println("+ , - , * , / , ^ , oder exit");
+            }
+            System.out.print(message + ": ");
+            operator = sc.nextLine();
+            firstTry = false;
+        }
+
+        return operator;
+    }
+
+    public static String readText(String message) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(message);
+        return sc.nextLine();
+    }
+
+    public static float readZahl(String text) {
+        System.out.print(text + ": ");
+        Scanner sc = new Scanner(System.in);
+        float value = 0.0f;
+        try {
+            String line = sc.nextLine();
+            value = Float.valueOf(line);
+
+        } catch (NumberFormatException nfe) {
+            System.out.println("Deine Zahl ist ungültig!!!");
+        }
+
+        return value;
+    }
+
+    public static boolean readJaNein(String message) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(message);
+        String answer = sc.nextLine();
+
+        return answer.equals("ja") || answer.equals("Ja") || answer.equals("j") || answer.equals("J");
+    }
+}
+
---
+++ src/homework/sandro/guessinggame/RateSpiel.java
@@ -0,0 +1,64 @@
+package homework.sandro.guessinggame;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RateSpiel {
+    static Random rand = new Random();
+
+    public static void main(String[] args) {
+        String name = readText("Wie ist dein Name?");
+        System.out.println("Willkommen " + name + " beim super coolen Zahlen Ratespiel ");
+        System.out.println("Du hast nur 5 versuche!");
+        int randomValue = rand.nextInt(101);
+        //  System.out.println(randomValue);
+
+        int i = 0;
+        int nummer = -1;
+        while ((i < 5) && (nummer != randomValue)) {
+            nummer = spielerZahl("Was ist deine Nummer", 0, 100);
+            if (nummer < randomValue) {
+                System.out.println("deine Zahl ist zu niedrig");
+            } else if (nummer > randomValue) {
+                System.out.println("deine Zahl ist zu hoch");
+            }
+            ++i;
+        }
+        if (nummer == randomValue) {
+            System.out.println("du hast gewonnen!!!");
+        } else {
+            System.out.println("Looser!\nDie richtige Zahl war: " + randomValue);
+        }
+        System.out.println("Danke fürs Spielen");
+    }
+
+    public static String readText(String message) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(message);
+        return sc.nextLine();
+    }
+
+    public static int spielerZahl(String message, int minValue, int maxValue) {
+        Scanner sc = new Scanner(System.in);
+        int value = 0;
+        boolean numberIsValid = false;
+        while (!numberIsValid) {
+            System.out.println(message);
+            try {
+                String line = sc.nextLine();
+                value = Integer.valueOf(line);
+                if ((value >= minValue) && (value <= maxValue)) {
+                    numberIsValid = true;
+                } else {
+                    System.out.println("Die Zahl muss zwischen " + minValue + " und " + maxValue + " sein ");
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("Deine Zahl ist ungültig!!!");
+            }
+        }
+        return value;
+    }
+
+
+}
+
